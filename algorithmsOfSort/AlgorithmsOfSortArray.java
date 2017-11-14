be2b45996d64ee05868f3d3d1a355c273f396afc
Vanya-B <babichevmail@gmail.com>
2017-11-14 15:26:29 +0200

    algorithm of bubble sort

--- algorithmsOfSort/AlgorithmsOfSortArray.java
+++ algorithmsOfSort/AlgorithmsOfSortArray.java
@@ -1,5 +1,18 @@
 package Algorithms.algorithmsOfSort;
 
 public class AlgorithmsOfSortArray {
-    
+
+    public static void bubbleSort (int [] array) {
+        if (array == null) throw new IllegalArgumentException();
+        if (array.length == 1) return;
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0, k = 1; j < i; j++, k++) {
+                if (array[j] > array[k]) {
+                    int tmp = array[j];
+                    array[j] = array[k];
+                    array[k] = tmp;
+                }
+            }
+        }
+    }
 }
