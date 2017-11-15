123b8836b21af758e4f783e7af3db2019b744687
Vanya-B <babichevmail@gmail.com>
2017-11-15 19:35:12 +0200

    algorithm of insertion sort

--- algorithmsOfSort/AlgorithmsOfSortArray.java
+++ algorithmsOfSort/AlgorithmsOfSortArray.java
@@ -31,4 +31,18 @@ public class AlgorithmsOfSortArray {
             array[index] = tmp;
         }
     }
+
+    public static void insertionSort (int [] array) {
+        if (array == null) throw new IllegalArgumentException();
+        if (array.length == 1) return;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+            }
+        }
+    }
 }
